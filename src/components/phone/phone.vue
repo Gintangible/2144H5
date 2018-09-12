f74b21c3138f6da7c64878469b0ddabfcbe7fb8a
gintangible <gintangible@163.com>
2018-09-12 23:19:35 +0800

    @change is not ok

--- src/components/phone/phone.vue
+++ src/components/phone/phone.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    phone
+  <div class="phone-wrap">
+    <div class="tel">
+      手机号：<input id="tel" type="tel" v-model="tel" @change="telVef()">
+    </div>
+    <div class="tel-vef">
+      验证码：<input type="text">
+      <i class="sent-code" :class="{act: phoneState}" @click="sendCode">发送验证码</i>
+    </div>
+    <a class="sent-btn" :class="{act: verState}" @click="verSuc">下一步</a>
   </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {};
+        return {
+            tel: '',
+            phoneState: false,
+            verState: false
+        };
     },
 
     components: {},
@@ -16,8 +27,95 @@ export default {
 
     mounted() {},
 
-    methods: {}
+    methods: {
+        telVaf() {
+            console.log(this.tel);
+            if (this.tel) {
+                this.phoneState = true;
+            }
+        },
+        sendCode() {
+            if (!this.phoneState) return;
+
+            setTimeout(() => {
+                this.verState = true;
+            }, 300);
+        },
+        verSuc() {
+            if (!this.verState) return;
+
+            setTimeout(() => {
+                alert('bind phone is ok');
+            }, 300);
+        }
+    }
 };
 </script>
 <style lang='scss' scoped>
+@import '../../common/sass/index.scss';
+
+.phone-wrap {
+    padding-top: 0.6rem;
+    width: 100%;
+    height: 100%;
+    background: #f5f5fa;
+    .tel,
+    .tel-vef {
+        @extend %pr;
+        padding-left: 0.3rem;
+        @include singleHeight(1.04);
+        font-size: 0.32rem;
+        color: #333;
+        border-top: 1px solid #d7d7d7;
+        background: #fff;
+        input {
+            @extend %pa;
+            left: 1.7rem;
+            top: 0;
+            width: 70%;
+            border: 0;
+            height: 1.04rem;
+            font-size: 0.32rem;
+            color: #333;
+        }
+    }
+    .tel-vef {
+        border-bottom: 1px solid #d7d7d7;
+        input {
+            width: 40%;
+        }
+        .sent-code {
+            @extend %pa;
+            margin-top: -0.4rem;
+            top: 50%;
+            right: 0.5rem;
+            width: 2.2rem;
+            color: #666;
+            @extend %tac;
+            font-size: 0.24rem;
+            @include singleHeight(0.8);
+            background: #ebebeb;
+            border-radius: 0.1rem;
+            &.act {
+                @extend %cfff;
+                background: #0bacff;
+            }
+        }
+    }
+    .sent-btn {
+        @extend %db;
+        margin: 0.3rem auto;
+        width: 90%;
+        font-size: 0.32rem;
+        @include singleHeight(1.06);
+        @extend %tac;
+        color: #666;
+        background: #ebebeb;
+        border-radius: 0.1rem;
+        &.act {
+            @extend %cfff;
+            background: #0bacff;
+        }
+    }
+}
 </style>
--- src/components/repwd/repwd.vue
+++ src/components/repwd/repwd.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>repwd</div>
+    <div class="repwd-wrap">
+        <span @click="repwd">修改密码</span>
+    </div>
 </template>
 
 <script>
@@ -14,8 +16,26 @@ export default {
 
     mounted() {},
 
-    methods: {}
+    methods: {
+        repwd() {
+            alert('repwd');
+        }
+    }
 };
 </script>
 <style lang='scss' scoped>
+@import '../../common/sass/index.scss';
+.repwd-wrap {
+    @extend %df;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    span {
+        padding: 0.2rem 0.4rem;
+        font-size: 0.4rem;
+        border: 1px solid #ccc;
+        border-radius: 0.4rem;
+    }
+}
 </style>
